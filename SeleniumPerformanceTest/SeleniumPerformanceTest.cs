6b4120067f2ebb545ea0c3bb42ccd6e59550e7e9
jbrammer@callibrity.com <jbrammer@callibrity.com>
2018-01-05 14:45:32 -0500

    initial commit

---
+++ SeleniumPerformanceTest/SeleniumPerformanceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
+namespace SeleniumPerformanceTest
+{
+    class SeleniumPerformanceTest
+    {
+        static void Main(string[] args)
+        {
+            Test(new ChromeDriver());
+        }
+
+        public static void Test(IWebDriver driver)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://automationpractice.com";
+        }
+    }
+}
